b5c79a841751e9a1f00a5a594577dd33a680dc29
Jeff Penketh <jeffpenketh@gmail.com>
2020-11-22 14:16:14 +0000

    ui fixes

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { API } from "aws-amplify";
-import { ListGroup } from "react-bootstrap";
-// import { LinkContainer } from "react-router-bootstrap";
+import { Fade } from "react-bootstrap";
 import { useAppContext } from "../libs/contextLib";
 import { onError } from "../libs/errorLib";
 import "./Home.css";
@@ -10,7 +9,30 @@ import "./Home.css";
 export default function Home() {
   const [ratings, setRatings] = useState([]);
   const { isAuthenticated, setHasRatings } = useAppContext();
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoadingRatings, setIsLoadingRatings] = useState(true);
+  const [isLoadingMovies, setIsLoadingMovies] = useState(true);
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    if (!ratings.length) {
+      return;
+    }
+
+    const promises = ratings.map(({ movieId }) =>
+      API.get("movies", `/movie/${movieId}/`)
+    );
+    async function getMovies() {
+      try {
+        const movies = await Promise.all(promises);
+        setMovies(movies);
+      } catch (e) {
+        onError(e);
+      } finally {
+        setIsLoadingMovies(false);
+      }
+    }
+    getMovies();
+  }, [ratings]);
 
   useEffect(() => {
     async function onLoad() {
@@ -21,12 +43,16 @@ export default function Home() {
       try {
         const ratings = await loadRatings();
         setRatings(ratings);
-        setHasRatings(ratings.length > 0);
+        const hasRatings = ratings.length > 0;
+        setHasRatings(hasRatings);
+        if (!hasRatings) {
+          setIsLoadingMovies(false);
+        }
       } catch (e) {
         onError(e);
+      } finally {
+        setIsLoadingRatings(false);
       }
-
-      setIsLoading(false);
     }
 
     onLoad();
@@ -36,25 +62,54 @@ export default function Home() {
     return API.get("movies", "/ratings");
   }
 
-  function renderRatingsList(ratings) {
-    // <p>Step 1: rate some movies so we know what you like.</p>
-    //     <p>
-    //       Step 2: get movie recommendations base on what similar users like.
-    //     </p>
-
-    return [{}].concat(ratings).map((rating, i) => (
-      <ListGroup.Item key={rating.createdAt} header={rating.rating}>
-        {"Movie: " + rating.movieId},
-        {"Created: " + new Date(rating.createdAt).toLocaleString()}
-      </ListGroup.Item>
-    ));
+  function renderRatingsList() {
+    return (
+      <div className="home-movies-list">
+        <h1>Your ratings</h1>
+        <Fade in={true} appear>
+          <ul>
+            {movies.length > 0 &&
+              movies.map(({ id, poster_path }) => {
+                const rating = ratings.find((r) => r.movieId === id.toString())
+                  .rating;
+                return (
+                  <li
+                    key={id}
+                    style={{
+                      position: "relative",
+                    }}>
+                    <div
+                      onClick={() => {}}
+                      style={{
+                        backgroundImage: `url(https://image.tmdb.org/t/p/w342/${poster_path})`,
+                      }}
+                    />
+                    <span className="rating-value">
+                      {[...Array(rating)].map((_el, i) => (
+                        <span key={i} style={{ padding: "3px" }}>
+                          &#11088;
+                        </span>
+                      ))}
+                    </span>
+                  </li>
+                );
+              })}
+          </ul>
+        </Fade>
+        {!isLoadingRatings && !isLoadingMovies && movies.length === 0 && (
+          <div>
+            <Link to="/movies">Go rate some movies</Link> to get recommendations
+          </div>
+        )}
+      </div>
+    );
   }
 
   function renderLander() {
     return (
       <div className="lander">
         <h1>Movie stuff</h1>
-        <p>A simple movie recommender app</p>
+        <p>Get some movie recommendations</p>
         <div className="pt-3">
           <Link to="/login" className="btn btn-info btn-lg mr-3">
             Login
@@ -67,18 +122,9 @@ export default function Home() {
     );
   }
 
-  function renderRatings() {
-    return (
-      <div className="notes">
-        <h1>Your Reviews</h1>
-        <ListGroup>{!isLoading && renderRatingsList(ratings)}</ListGroup>
-      </div>
-    );
-  }
-
   return (
     <div className="Home">
-      {isAuthenticated ? renderRatings() : renderLander()}
+      {isAuthenticated ? renderRatingsList() : renderLander()}
     </div>
   );
 }
